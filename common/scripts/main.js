ecbbb8958559e406fd682afdca8d5f86ce2f7d1f
Jinsu Mathew <jinsu.mails@gmail.com>
2024-01-26 11:26:24 -0600

    Updates resize logic

--- common/scripts/main.js
+++ common/scripts/main.js
@@ -7,45 +7,49 @@ function initZDApp() {
     console.log("Deal: \n" + JSON.stringify(obj, null, 4))
     dealID = obj.deal.id
   });
-  
+
   window.addEventListener('message', event => {
-    // IMPORTANT: check the origin of the data! 
+    // IMPORTANT: check the origin of the data!
     if (event.origin.startsWith(remoteURL)) {
       // The data was sent from your site.
       console.log("Query:\n" + JSON.stringify(event.data, null, 4));
       const iframe = document.querySelector("iframe");
       if (event.data.type === 'height') {
-        client.invoke('resize', { width: '100%', height: event.data.height + 'px' });
+        let height = window.innerHeight;
+        if (event.data.height && event.data.height > window.innerHeight) {
+          height = event.data.height;
+        }
+        client.invoke('resize', { width: '100%', height: height + 'px' });
       } else if (event.data.type === 'context') {
         client.context().then(function(obj) {
-          iframe.contentWindow.postMessage({'type': event.data.type, 'obj': obj}, remoteURL)          
+          iframe.contentWindow.postMessage({ 'type': event.data.type, 'obj': obj }, remoteURL)
         });
       } else if (event.data.type === 'post') {
         client.request({
-            url: '/v2/notes',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({
-              "data": {
-                "resource_type": "deal",
-                "resource_id": dealID,
-                "content": event.data.note,
-                "is_important": false,
-                "tags": event.data.tags,
-                "type": "regular"
-              },
-              "meta": {
-                "type": "note"
-              }
-            })
-          }).then(function(data) {
-            iframe.contentWindow.postMessage({'type': 'posted', 'obj': data}, remoteURL)
-          }).catch(function(error) {
-            console.log("Notes error: " + error.toString()); // "APIUnavailable: "nonExistentPath" Could not find handler for: "nonExistentPath"
+          url: '/v2/notes',
+          type: 'POST',
+          contentType: 'application/json',
+          data: JSON.stringify({
+            "data": {
+              "resource_type": "deal",
+              "resource_id": dealID,
+              "content": event.data.note,
+              "is_important": false,
+              "tags": event.data.tags,
+              "type": "regular"
+            },
+            "meta": {
+              "type": "note"
+            }
           })
+        }).then(function(data) {
+          iframe.contentWindow.postMessage({ 'type': 'posted', 'obj': data }, remoteURL)
+        }).catch(function(error) {
+          console.log("Notes error: " + error.toString()); // "APIUnavailable: "nonExistentPath" Could not find handler for: "nonExistentPath"
+        })
       } else {
         client.get(event.data.type).then(function(obj) {
-          iframe.contentWindow.postMessage({'type': event.data.type, 'obj': obj}, remoteURL)          
+          iframe.contentWindow.postMessage({ 'type': event.data.type, 'obj': obj }, remoteURL)
         });
       }
     } else if (event.origin.startsWith("https://app.futuresimple.com")) {
@@ -56,12 +60,12 @@ function initZDApp() {
         iframe.contentWindow.postMessage(event.data, remoteURL)
       }
     } else {
-      // The data was NOT sent from your site! 
+      // The data was NOT sent from your site!
       console.log("Claims commander container unknown origin - " + event.origin + "\n" + JSON.stringify(event.data, null, 4))
       return;
     }
   });
-  
+
   // Init the iframe
   document.getElementById("mainContent").src = remoteURL + remotePath
 }
